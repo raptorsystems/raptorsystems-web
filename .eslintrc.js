0a3a0f91ebb955725737b22fadb8491cfec8bcca
Sebastian Aranguiz <sarang575@gmail.com>
2019-11-05 12:11:16 -0300

    Update eslint and ts configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,20 +4,38 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2019,
     sourceType: 'module',
+    project: 'tsconfig.json',
+    extraFileExtensions: ['.vue'],
   },
 
   extends: [
+    // Base ESLint recommended rules
+    'eslint:recommended',
+    // ESLint typescript rules
+    // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#usage
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    // Nuxt
+    // https://typescript.nuxtjs.org/guide/lint.html#lint
     '@nuxtjs/eslint-config-typescript',
+    // Prettier
+    // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#usage-with-prettier
     'prettier',
     'prettier/@typescript-eslint',
     'prettier/vue',
-    'plugin:prettier/recommended',
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
 
   plugins: ['vuetify'],
 
   rules: {
+    'require-atomic-updates': 'off',
+    // @typescript-eslint
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/unbound-method': 0,
+    // vuetify
     'vuetify/no-deprecated-classes': 'error',
     'vuetify/grid-unknown-attributes': 'error',
     'vuetify/no-legacy-grid': 'error',
