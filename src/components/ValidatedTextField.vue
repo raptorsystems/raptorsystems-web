2ae646e7a20fe261520c37733bc187691cbf6830
Sebastian Aranguiz <sarang575@gmail.com>
2019-11-05 12:12:17 -0300

    Add ValidatedTextField component

---
+++ src/components/ValidatedTextField.vue
@@ -0,0 +1,60 @@
+<template>
+  <ValidationProvider
+    v-slot="{ errors, valid }"
+    :name="validationName"
+    :vid="$attrs.vid"
+    :rules="rules"
+    slim
+  >
+    <v-text-field
+      v-model="innerValue"
+      :error-messages="errors"
+      :success="valid"
+      v-bind="$attrs"
+      v-on="$listeners"
+    ></v-text-field>
+  </ValidationProvider>
+</template>
+
+<script>
+// components
+import { ValidationProvider } from 'vee-validate'
+
+export default {
+  name: 'ValidatedTextField',
+  components: {
+    ValidationProvider,
+  },
+  inheritAttrs: false,
+  props: {
+    rules: { type: [Object, String], default: '' },
+    // must be included in props
+    value: { type: null, default: undefined },
+  },
+  data() {
+    return {
+      innerValue: undefined,
+    }
+  },
+  computed: {
+    validationName() {
+      const { label } = this.$attrs
+      return label && label.toLowerCase()
+    },
+  },
+  watch: {
+    // Handles external model changes
+    value(newVal) {
+      this.innerValue = this.formatValue(newVal)
+    },
+  },
+  created() {
+    this.innerValue = this.formatValue(this.value)
+  },
+  methods: {
+    formatValue(value) {
+      return value
+    },
+  },
+}
+</script>
