cc461a186857f4858053b9095cf1c5fe67fbd8f6
Sebastian Aranguiz <sarang575@gmail.com>
2020-08-08 01:42:12 -0400

    Use font-display swap for webfontloader

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -92,8 +92,14 @@ const config: NuxtConfig = {
   },
 
   webfontloader: {
-    google: {
-      families: ['Raleway:100,400,500', 'Lato:300,400,500'],
+    // Workarounf for adding font-display option
+    // https://github.com/Developmint/nuxt-webfontloader#adding-google-fonts-with-font-display-option
+    custom: {
+      families: ['Raleway:n1,n4,n5', 'Lato:n3,n4,n5'], // uses FVD notaton https://github.com/typekit/fvd
+      urls: [
+        'https://fonts.googleapis.com/css?family=Raleway:100,400,500&display=swap',
+        'https://fonts.googleapis.com/css?family=Lato:300,400,500&display=swap',
+      ],
     },
   },
 }
