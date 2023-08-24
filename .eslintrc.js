b1dac0ad8606a500f694680cddeaf551d5a7b7e2
Sebastian Aranguiz <sarang575@gmail.com>
2023-08-24 17:38:05 -0400

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,9 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-
-  parser: 'vue-eslint-parser',
-
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    // tsconfigRootDir: __dirname,
-    // project: ['./tsconfig.json', './tsconfig.eslint.json'],
-    extraFileExtensions: ['.vue'],
-  },
-
   extends: [
-    // Base ESLint recommended rules
-    'eslint:recommended',
-    // Vue 3
-    'plugin:vue/vue3-recommended',
-    // ESLint typescript rules
-    // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#usage
-    'plugin:@typescript-eslint/recommended',
-    // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    // Nuxt
-    // https://typescript.nuxtjs.org/guide/lint.html#lint
-    '@nuxtjs/eslint-config-typescript',
-    // Prettier
-    'plugin:prettier/recommended',
+    '@nuxt/eslint-config',
+    'plugin:vuetify/recommended',
+    'prettier',
   ],
-
-  plugins: ['vue', '@typescript-eslint'],
-
-  // plugins: ['vuetify'],
-
-  rules: {
-    // @typescript-eslint
-    // '@typescript-eslint/camelcase': 'off',
-    // vue
-    // 'vue/html-self-closing': ['error', { html: { void: 'always' } }],
-    'vue/multi-word-component-names': 'off',
-    // vuetify
-    // 'vuetify/no-deprecated-classes': 'error',
-    // 'vuetify/grid-unknown-attributes': 'error',
-    // 'vuetify/no-legacy-grid': 'error',
-  },
 }
